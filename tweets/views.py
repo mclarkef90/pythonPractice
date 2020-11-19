d519c9b10ffec5bef4833f89d165d30c5248a9c5
morgancfarrow90 <63519344+morgancfarrow90@users.noreply.github.com>
2020-11-19 15:11:52 -0500

    created index page

--- tweets/views.py
+++ tweets/views.py
@@ -6,7 +6,15 @@ from .models import Tweet
 # Create your views here.
 
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "response": tweets_list
+    }
+    return JsonResponse(data)
 
 def tweets_view(request, tweet_id, *args, **kwargs):
     """
@@ -14,12 +22,14 @@ def tweets_view(request, tweet_id, *args, **kwargs):
     Purpose so we can consume by Javascript, or java, swift,etc.
     return json data
     """
+    data= {
+        "id": tweet_id,
+    }
+    status = 200
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        data['content']= obj.content
     except: 
-        raise Http404
-    data={
-        "id": tweet_id,
-        "content": obj.content
-    }
-    return JsonResponse(data)
+        data['message'] = 'Not Found'
+        status = 404
+    return JsonResponse(data, status=status)
