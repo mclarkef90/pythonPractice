28c3166559eb94c2edb6c8e98adaa0840736e592
morgancfarrow90 <63519344+morgancfarrow90@users.noreply.github.com>
2020-11-20 13:02:08 -0500

    create tweet form added

--- tweets/views.py
+++ tweets/views.py
@@ -2,15 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
 
 from .models import Tweet
+from .forms import TweetForm
 
 # Create your views here.
 
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        form = TweetForm()
+    return render(request, "components/form.html", context={"form": form})
+
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    tweets_list = [{"id": x.id, "content": x.content, "likes": 0} for x in qs]
     data = {
         "response": tweets_list
     }
